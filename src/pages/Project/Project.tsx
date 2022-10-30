3440c6efe6fc418e97813abfff06f0130965b6ce
Luka <srps8912@gmail.com>
2022-10-30 23:27:28 +0800

    Feat: Build Lists and Cards by dummy data

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -1,9 +1,27 @@
 import styled from "styled-components";
+import List from "./components/List";
 
 const Wrapper = styled.div``;
 
+const ListWrapper = styled.div`
+  display: flex;
+`;
+
+const LISTS = [
+  { title: "List1", cards: [{ title: "card1" }, { title: "card2" }] },
+];
+
 const Project = () => {
-  return <Wrapper>Project1 </Wrapper>;
+  return (
+    <Wrapper>
+      Project1
+      <ListWrapper>
+        {LISTS.map((list) => {
+          return <List title={list.title} cards={list.cards} />;
+        })}
+      </ListWrapper>
+    </Wrapper>
+  );
 };
 
 export default Project;
