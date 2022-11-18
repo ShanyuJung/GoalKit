080fc618faf3d0687f7d5262ab5622c10d65504b
Luka <srps8912@gmail.com>
2022-11-18 14:34:49 +0800

    Fix: Card tag labels will cover sidebar

--- src/pages/Project/components/ProjectSidebar.tsx
+++ src/pages/Project/components/ProjectSidebar.tsx
@@ -20,6 +20,7 @@ const SidebarWrapper = styled.div<StylesProps>`
   height: calc(100vh - 50px);
   transition: width 0.3s;
   position: fixed;
+  z-index: 10;
 `;
 
 const WorkspaceTitleWrapper = styled.div<StylesProps>`
